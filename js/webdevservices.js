4216668bc8f1a781293ca7b80acdc27dcf0ffb95
AlekyaKowta <akowta@gmail.com>
2020-07-16 20:42:47 +0530

    wds rotator content

---
+++ js/webdevservices.js
@@ -0,0 +1,72 @@
+function one() {
+	document.getElementById("content-one").style.display = "inline-block";
+	document.getElementById("content-two").style.display = "none";
+	document.getElementById("content-three").style.display = "none";
+	document.getElementById("content-four").style.display = "none";
+	document.getElementById("content-five").style.display = "none";
+	document.getElementById("content-six").style.display = "none";
+	document.getElementById("content-seven").style.display = "none";
+}
+function two() {
+	document.getElementById("content-two").style.display = "inline-block";
+	document.getElementById("content-one").style.display = "none";
+	document.getElementById("content-three").style.display = "none";
+	document.getElementById("content-four").style.display = "none";
+	document.getElementById("content-five").style.display = "none";
+	document.getElementById("content-six").style.display = "none";
+	document.getElementById("content-seven").style.display = "none";
+}
+function three() {
+	document.getElementById("content-three").style.display = "inline-block";
+	document.getElementById("content-two").style.display = "none";
+	document.getElementById("content-one").style.display = "none";
+	document.getElementById("content-four").style.display = "none";
+	document.getElementById("content-five").style.display = "none";
+	document.getElementById("content-six").style.display = "none";
+	document.getElementById("content-seven").style.display = "none";
+}
+function four() {
+	document.getElementById("content-four").style.display = "inline-block";
+	document.getElementById("content-two").style.display = "none";
+	document.getElementById("content-three").style.display = "none";
+	document.getElementById("content-one").style.display = "none";
+	document.getElementById("content-five").style.display = "none";
+	document.getElementById("content-six").style.display = "none";
+	document.getElementById("content-seven").style.display = "none";
+}
+function five() {
+	document.getElementById("content-five").style.display = "inline-block";
+	document.getElementById("content-two").style.display = "none";
+	document.getElementById("content-three").style.display = "none";
+	document.getElementById("content-four").style.display = "none";
+	document.getElementById("content-one").style.display = "none";
+	document.getElementById("content-six").style.display = "none";
+    document.getElementById("content-seven").style.display = "none";
+}
+function six() {
+	document.getElementById("content-six").style.display = "inline-block";
+	document.getElementById("content-two").style.display = "none";
+	document.getElementById("content-three").style.display = "none";
+	document.getElementById("content-four").style.display = "none";
+	document.getElementById("content-five").style.display = "none";
+	document.getElementById("content-one").style.display = "none";
+	document.getElementById("content-seven").style.display = "none";
+}
+function seven() {
+	document.getElementById("content-seven").style.display = "inline-block";
+	document.getElementById("content-two").style.display = "none";
+	document.getElementById("content-three").style.display = "none";
+	document.getElementById("content-four").style.display = "none";
+	document.getElementById("content-five").style.display = "none";
+	document.getElementById("content-six").style.display = "none";
+	document.getElementById("content-one").style.display = "none";
+}
+function eight() {
+	document.getElementById("content-eight").style.display = "inline-block";
+	document.getElementById("content-two").style.display = "none";
+	document.getElementById("content-three").style.display = "none";
+	document.getElementById("content-four").style.display = "none";
+	document.getElementById("content-five").style.display = "none";
+	document.getElementById("content-six").style.display = "none";
+	document.getElementById("content-seven").style.display = "none";
+}
